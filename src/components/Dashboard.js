ac4c11caf0d498fd5239a158b7bd5d8cd3dafbb8
Nicolas Ranz <nranz1410@hotmail.com>
2023-03-12 18:12:55 -0400

    updated and added authentication

---
+++ src/components/Dashboard.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Card, Button, Alert } from 'react-bootstrap'
+import { Link, useNavigate } from 'react-router-dom'
+import {useAuth} from '../contexts/AuthContext1'
+import {getAuth, signOut} from "firebase/auth"
+
+export default function Dashboard() {
+  const [error, setError] = useState("")
+  const { currentUser, logout }= useAuth()
+  const navigate = useNavigate()
+  const baby = getAuth()
+
+
+  async function handleLogout(){
+    setError("")
+
+    try {
+      await signOut(baby)
+      navigate('/login')
+    } catch (error) {
+      console.log(error)
+      setError("Failed to logout")
+    }
+  }
+
+  return (
+    <> 
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>
+            Profile
+          </h2>
+          {error && <Alert variant = "danger">{error}</Alert>}
+          <strong>Email:</strong> {currentUser.email}
+          <Link to = "/update-profile" className= "btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className=' w-100 text-center mt-2'>
+        <Button variant = "link" onClick = {handleLogout}>Log Out</Button>
+      </div>
+    </>
+    
+  )
+}
