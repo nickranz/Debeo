8e634045232cdbf671d396a11621ae2288182b5c
Reva-h <reva.hirave@gmail.com>
2023-04-07 11:37:38 -0400

    added config.js file and firebase import statements to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./components/App1";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Firebase from 'firebase';
+import config from './config';
 
 ReactDOM.render(
   <React.StrictMode>
