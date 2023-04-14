b4906dfab293e202eb5066dca3f408effc6db381
Nicolas Ranz <nranz1410@hotmail.com>
2023-04-14 11:30:46 -0400

    Renamed task to TransactionTask

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "./Navigation";
 import { Container } from "react-bootstrap";
-import Task from "./Task";
+import Task from "./TransactionTask";
 
 const HomeScreen = () => {
   return (
---
+++ src/components/TransactionTask.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card, Container, Row, Col } from "react-bootstrap";
+import reciept from "../exampleData/data2.json";
+
+export default function Task() {
+  return (
+    <Card style={{ width: "29rem" }}>
+      {/* <Card.Header className=" text-center">I'm going to be a task</Card.Header> */}
+      <Card.Header>This is user's transactions task.</Card.Header>
+      <Container>
+        <Row>
+          <Col> Nick paid Mike {reciept.total}</Col>
+          {/* <Col>Name of User</Col>
+          <Col>{reciept.description}</Col>
+          <Col>Total Cost of Item {reciept.total}</Col>
+          <Col>Details</Col> */}
+        </Row>
+        <Row>
+          <Col style = {{fontSize : "10px"}}>{reciept.date}</Col>
+        </Row>
+        <Row>
+          <Col style = {{fontSize : "10px"}}>{reciept.description}</Col>
+        </Row>
+      </Container>
+    </Card>
+  );
+}
