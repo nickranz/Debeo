2d9a7b8bfb5dd0cdef5e75c357125e93410c9835
mcrchavez <mcrchavez713@gmail.com>
2023-04-22 21:49:26 -0400

    Added database initialization to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase/compat/app'
 import "firebase/compat/auth"
+import { getDatabase } from "firebase/database";
+
 
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -8,7 +10,9 @@ const app = firebase.initializeApp({
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
+    databaseURL: "https://debeo-development-default-rtdb.firebaseio.com/:DB"
 })
 
+export const database = getDatabase(app)
 export const auth = app.auth()
 export default app
