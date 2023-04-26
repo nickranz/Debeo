0a0fba54652fb67ac4f39181d93918ecf4269f8a
Reva-h <reva.hirave@gmail.com>
2023-04-26 15:44:14 -0400

    Added functions.js file

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const config = {
+    apiKey: "AIzaSyAsEoHtRCzFA-u34nyBOCqF3lHt2m-iecs",
+    authDomain: "debeo-development.firebaseapp.com",
+    databaseURL: "https://debeo-development-default-rtdb.firebaseio.com/"
+  }
+  
+  export default config;
