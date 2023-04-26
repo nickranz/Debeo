6273ee09831ee227359d80a5b9aefe6b5c6fb1a3
Reva-h <reva.hirave@gmail.com>
2023-04-26 16:50:39 -0400

    commented out OO functions, added headers for add + removes

--- src/functions.js
+++ src/functions.js
@@ -1,60 +1,90 @@
 import "./db.js"
 import "./firebase.js"
 
-class Team{
-    name;
-    admin;
+// class Team{
+//     name;
+//     admin;
     
-    Team(name){
-        this.name = name;
-        this.admin = null;
-    }
+//     Team(name){
+//         this.name = name;
+//         this.admin = null;
+//     }
 
-    setName(name){
+//     setName(name){
 
-    }
-    getName(){
-        return this.name;
-    }
-    setAdmin(adminUser){
+//     }
+//     getName(){
+//         return this.name;
+//     }
+//     setAdmin(adminUser){
 
-    }
-    getAdmin(){
-        return this.admin;
+//     }
+//     getAdmin(){
+//         return this.admin;
 
-    }
+//     }
 
-    addMember(user){
+//     addMember(user){
 
-    }
-} 
-    class Item {
-        constructor(name, cost, quantity) {
-          this.name = name;
-          this.cost = cost;
-          this.quantity = quantity;
-        }
-        getName() {
-            return this.name;
-        }
-        getCost() {
-            return this.cost;
-        }
-        getQuantity() {
-            return this.quantity;
-        }
-        setName(newName) {
-            this.name = newName;
-            return;
-        }
-        setCost(newCost) {
-            this.cost = newCost;
-            return;
-        }
-        setQuantity(newQuantity) {
-            this.quantity = newQuantity;
-            return this.name;
-        }
+//     }
+// } 
+//     class Item {
+//         constructor(name, cost, quantity) {
+//           this.name = name;
+//           this.cost = cost;
+//           this.quantity = quantity;
+//         }
+//         getName() {
+//             return this.name;
+//         }
+//         getCost() {
+//             return this.cost;
+//         }
+//         getQuantity() {
+//             return this.quantity;
+//         }
+//         setName(newName) {
+//             this.name = newName;
+//             return;
+//         }
+//         setCost(newCost) {
+//             this.cost = newCost;
+//             return;
+//         }
+//         setQuantity(newQuantity) {
+//             this.quantity = newQuantity;
+//             return this.name;
+//         }
     
-      }
-    
+//       }
+
+function addTeam(teamName){
+
+}
+function addMember(teamName, memberName){
+
+}
+function removeTeam(teamName){
+
+}
+function removeMember(teamName, memberName){
+
+}
+function setAdmin(teamName, memberName){
+
+}
+function unsetAdmin(teamName, memberName){
+
+}
+function addTransaction(teamName, memberName){
+
+}
+function addItem(teamName, memberName){
+
+}
+function removeTransaction(teamName, memberName){
+
+}
+function removeItem(teamName, memberName){
+
+}
