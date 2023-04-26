7c9fad13122354f988869062a2ee1ef057af05ee
Reva-h <reva.hirave@gmail.com>
2023-04-26 16:10:16 -0400

    added item class to functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,32 @@
 import "./db.js"
+import "./firebase.js"
+
+class Item {
+    constructor(name, cost, quantity) {
+      this.name = name;
+      this.cost = cost;
+      this.quantity = quantity;
+    }
+    getName() {
+        return this.name;
+    }
+    getCost() {
+        return this.cost;
+    }
+    getQuantity() {
+        return this.quantity;
+    }
+    setName(newName) {
+        this.name = newName;
+        return;
+    }
+    setCost(newCost) {
+        this.cost = newCost;
+        return;
+    }
+    setQuantity(newQuantity) {
+        this.quantity = newQuantity;
+        return this.name;
+    }
+
+  }
