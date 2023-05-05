cf54273011b33b13188484a145e8c635e5052b8a
Nicolas Ranz <nranz1410@hotmail.com>
2023-05-04 22:05:20 -0400

    added button to add reciept but button is failing to add more items to page

--- src/components/AddReciept.js
+++ src/components/AddReciept.js
@@ -1,14 +1,27 @@
 import React, { useRef, useState } from "react";
-import {Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext1";
 import NavBarDash from "./NavBarDash";
-import ItemInfo from "./ItemInfo"
+import ItemInfo from "./ItemInfo";
 
 export default function AddReciept() {
   const { currentUser } = useAuth();
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
+  const [receiptList, setreceiptList] = useState([]);
+  const AddMember = () => {
+    return <ItemInfo />;
+  };
+
+  const addBtnClick = () => {
+    if (receiptList.length < 6) {
+      setreceiptList(
+        receiptList.concat(<AddMember key={receiptList.length} />)
+      );
+    }
+  };
 
   return (
     <>
@@ -16,35 +29,63 @@ export default function AddReciept() {
         <NavBarDash />
       </div>
       <Container>
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-12 col-lg-10 col-xl-8 mx-auto">
-              <div class="my-4">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-lg-10 col-xl-8 mx-auto">
+              <div className="my-4">
                 <Form>
-                  <div class="row mt-1 align-items-center">
-                      <h4 class="mb-1">Add Receipt</h4>
-                    </div>
-                    <hr class="my-4" />
-                    <div class="form-row">
-                        <Form.Group class="col-md-6">
-                            <label for="date">Date Created</label>
-                            <input type="text" id="date" class="form-control" placeholder="05/08/2023" />
-                        </Form.Group>
-                        <div class="row mb-4" />
-                        <Form.Group class="col-md-6">
-                            <label for="merchant">Merchant</label>
-                            <input type="text" id="merchant" class="form-control" placeholder="Walmart" />
-                        </Form.Group>
-                        <div class="row mb-4" />
-                        <Form.Group class="col-md-6">
-                            <label for="purchaser">Purchased By</label>
-                            <input type="text" id="purchaser" class="form-control" placeholder="Jack Sparrow" />
-                        </Form.Group>
-                    </div>
-                    <ItemInfo />
-                    <div class="row mb-4"/>
-                  <Button disabled={loading} class="btn btn-primary" type="submit">
+                  <div className="row mt-1 align-items-center">
+                    <h4 className="mb-1">Add Receipt</h4>
+                  </div>
+                  <hr className="my-4" />
+                  <div className="form-row">
+                    <Form.Group className="col-md-6">
+                      <label id="date">Date Created</label>
+                      <input
+                        type="text"
+                        id="date"
+                        className="form-control"
+                        placeholder="05/08/2023"
+                      />
+                    </Form.Group>
+                    <div className="row mb-4" />
+                    <Form.Group className="col-md-6">
+                      <label id="merchant">Merchant</label>
+                      <input
+                        type="text"
+                        id="merchant"
+                        className="form-control"
+                        placeholder="Walmart"
+                      />
+                    </Form.Group>
+                    <div className="row mb-4" />
+                    <Form.Group className="col-md-6">
+                      <label id="purchaser">Purchased By</label>
+                      <input
+                        type="text"
+                        id="purchaser"
+                        className="form-control"
+                        placeholder="Jack Sparrow"
+                      />
+                    </Form.Group>
+                  </div>
+                  {receiptList}
+                  <ItemInfo />
+                  <div className="row mb-4" />
+                  <Button
+                    disabled={loading}
+                    className="btn btn-primary"
+                    type="submit"
+                  >
                     Submit
+                  </Button>
+                  <Button
+                    disabled={loading}
+                    type="submit"
+                    className="btn btn-primary mx-2"
+                    onClick={addBtnClick}
+                  >
+                    Add another item
                   </Button>
                 </Form>
               </div>
